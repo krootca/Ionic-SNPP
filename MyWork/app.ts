8b70d97f9277b6098120bcb9cb56ae41c180eec1
krootca <63897401+krootca@users.noreply.github.com>
2022-05-05 17:15:40 -0400

    Add files via upload

---
+++ MyWork/app.ts
@@ -0,0 +1,6 @@
+function viewSettings(){
+	document.getElementById('MySettings').style="color: black;position: absolute;display: inherit;top:230px;left:35%";
+}
+function CloseSettings(){
+	document.getElementById('MySettings').style="color: black;position: absolute;display: none;top:230px;left:35%";
+}
